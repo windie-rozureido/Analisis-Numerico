e887787f2d3a1fbd882bfe69dbeb5fb0468f260a
Gerardo Rivas A <windie@ciencias.unam.mx>
2019-10-06 23:49:35 -0500

    Se agregó el código para graficar error en calculo de derivadas

--- derivate.py
+++ derivate.py
@@ -5,6 +5,8 @@
 #########################################
 
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 #La sig. func. estima la derivada del seno.
 #usando diferencia hacia adelante.
@@ -15,15 +17,24 @@ def Dsin(x, h):
 #La sig. func. calcula la derivada del seno
 # usando diferencia centrada.
 
-
+h = np.zeros(15)
+error = np.zeros(15)
 
 for i in range(0,15):
-    h = 10**(-i) #Se define la 'h' del paso
+    h[i] = 10**(-i) #Se define la 'h' del paso
     x = math.pi/4 #Se define el pnto donde se deriva
-    aprox = Dsin(x, h) #Se calcula la aprox primer metodo
+    aprox = Dsin(x, h[i]) #Se calcula la aprox primer metodo
     real = math.cos(x) #Este es el valor real
-    error = (aprox-real)/real #Se calcula el error relatvo
+    error[i] = abs((aprox-real))/real #Se calcula el error relatvo
+
+fig, ax = plt.subplots()
+
+ax.set_yscale('log')
+ax.semilogx(h, error)
+ax.grid()
+ax
+plt.xlabel('h')
+plt.ylabel('error')
 
 
-    print(h, aprox, real, error) #Se tabula los resltds
-    
+plt.show()
--- derivate2.py
+++ derivate2.py
@@ -5,7 +5,8 @@
 #########################################
 
 import math
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 #La sig. func. calcula la derivada del seno
 # usando diferencia centrada.
@@ -14,12 +15,27 @@ def Dsin2(x,h):
     derivate = (math.sin(x+h)-math.sin(x-h))/(2*h)
     return derivate
 
+h = np.zeros(15)
+error = np.zeros(15)
+
 for i in range(0,15):
-    h = 10**(-i) #Se define la 'h' del paso
+    h[i] = 10**(-i) #Se define la 'h' del paso
     x = math.pi/4 #Se define el pnto donde se deriva
-    aprox = Dsin2(x,h) #Se calcula la aprox segundo metodo
+    aprox = Dsin2(x,h[i]) #Se calcula la aprox segundo metodo
     real = math.cos(x) #Este es el valor real
-    error = (aprox-real)/real #Se calcula el error relatvo
+    error[i] = abs((aprox-real))/real #Se calcula el error relatvo
 
 
-    print(h, aprox, real, error) #Se tabula los resltds
+   
+
+fig, ax = plt.subplots()
+
+ax.set_yscale('log')
+ax.semilogx(h, error)
+ax.grid()
+ax
+plt.xlabel('h')
+plt.ylabel('error')
+
+
+plt.show()
