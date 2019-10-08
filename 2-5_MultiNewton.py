5dd3bd596b19edf7b82a6ea6a334e60f77dde45f
Gerardo Rivas A <windie@ciencias.unam.mx>
2019-10-07 22:38:50 -0500

    Se dieron los puntos iniciales adecuados para encontrar las soluciones de interés

--- 2-5_MultiNewton.py
+++ 2-5_MultiNewton.py
@@ -1,4 +1,9 @@
 ########################################
+# 2-5 Newton Multidimensional          #
+# Este programa encuentra la inter-    #
+# sección de dos elipses.              #
+# Autora: Ofelia Cepeda Camargo        #
+########################################
 
 
 import numpy as np
@@ -22,19 +27,29 @@ def DF(x):
 
 # Valores iniciales del algoritmo
 
-x = np.array([100,150.0])
-s = np.array([0.0, 0.0])
+x1 = np.array([-1.0,3.0])
+s1 = np.array([0.0, 0.0])
+
+x2 = np.array([-10.0,-5.5])
+s2 = np.array([0.0, 0.0])
 
 # El bucle principal
 
-for n in range(1,31):
+for n in range(1,51):
 
-    A = DF(x)
-    B = - 1.0 * F(x)
-
-    s = np.linalg.solve(A, B) # El sistema lineal lo resuelve numpy
-
-    x = x + s
-  
-print("La colision es en el punto ", x)
+    A1 = DF(x1)
+    A2 = DF(x2)
+    
+    B1 = - 1.0 * F(x1)
+    B2 = - 1.0 * F(x2)
+    
+    s1 = np.linalg.solve(A1, B1) # El sistema lineal lo resuelve numpy
+    s2 = np.linalg.solve(A2, B2)
+    
+    x1 = x1 + s1
+    x2 = x2 + s2 
+    
+print("Posibles puntos de colisión: ")
+print("x_1 = ", x1)
+print("x_2 = ", x2)
   
--- 2-6_Parabolas.py
+++ 2-6_Parabolas.py
@@ -1,8 +1,8 @@
 ########################################
-# 2-6 Parabolas
-#
-# Autor: Ofelia Cepeda Camargo
-
+# 2-6 Parabolas                        #
+#                                      #
+# Autor: Ofelia Cepeda Camargo         #
+########################################
 
 import numpy as np
 
@@ -14,13 +14,32 @@ def DF(x):
   df = (np.array([[2*x[0], - 1], [- 1, 2 * x[1]]]))
   return df
 
-x = np.array([1,1.5])
-s = np.array([0.0, 0.0])
-for n in range(1,16):
-  A = DF(x)
-  B = -1.0*F(x)
-  s = np.linalg.solve(A, B)
-  x = x + s
+def Newton(x):
+    s = np.array([0.0, 0.0])
+    for n in range(1,16):
+        A = DF(x)
+        B = -1.0*F(x)
+        s = np.linalg.solve(A, B)
+        x = x + s
+    return x
+
+x1 = np.array([-0.61, -0.61])
+x2 = np.array([0.1, -1.1])
+x3 = np.array([-1.1, 0.1])
+x4 = np.array([1.61, 1.61])
+
+x1 = Newton(x1)
+
+x2 = Newton(x2)
+
+x3 = Newton(x3)
+
+x4 = Newton(x4)
   
-print("La colision es en el punto ", x)
+print("Los puntos de intersección de las parábolas son:")
+
+print("x_1 = ", x1)
+print("x_2 = ", x2)
+print("x_3 = ", x3)
+print("x_4 = ", x4)
   
